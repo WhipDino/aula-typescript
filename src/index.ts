21f08a83cee273e4e5dc41e228331b48c1baddc6
João <jvdemorais10@gmail.com>
2025-02-25 22:21:55 -0300

    Exercício 2 - Função Tipada

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,10 @@ console.log(mensagem);
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
+
+// Exercicio 2
+
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
