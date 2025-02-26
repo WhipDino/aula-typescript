25529776148970edb26d17a1f83a868a75a4a053
João <jvdemorais10@gmail.com>
2025-02-25 22:21:23 -0300

    Exercicio 1 - Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
 
+//Exercicio 1
+
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
